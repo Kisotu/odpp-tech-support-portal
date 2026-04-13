ac5955779ab37a0633042a53042cd1a48caa3ce2
Oloishorua <jacoboloishorua@gmail.com>
2026-04-13 12:25:26 +0300

    feat(frontend): add barrel exports for ticket components

---
+++ frontend/src/components/tickets/index.js
@@ -0,0 +1,5 @@
+export { default as TicketList } from './TicketList';
+export { default as TicketForm } from './TicketForm';
+export { default as TicketDetail } from './TicketDetail';
+export { default as TicketFilters } from './TicketFilters';
+export { default as CommentSection } from './CommentSection';
